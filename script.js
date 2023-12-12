20f3c02eb67f8527c2883346381829db008e80e4
Roy-Ankush <akr022003@gmail.com>
2023-12-12 18:00:28 +0530

    Add initial files

---
+++ script.js
@@ -0,0 +1,21 @@
+// ******************this is for signup and i am using java script here**************** 
+
+    document.querySelector("#sig").addEventListener("click",show_signup);
+function show_signup() {
+    document.querySelector(".left .login").style.display="none";
+    document.querySelector(".left #btn0").style.display="block";
+    document.querySelector(".left #btn1").style.display="block";
+    document.querySelector(".right #btn20").style.display="none";
+    document.querySelector(".right #sig").style.display="none";
+    document.querySelector(".right .signup").style.display="block";
+
+}
+// ******************this is for login nad i am using jquery here****************
+$("#btn1").on("click",show_login); //instead of show_login we will also write some annonimus function also
+    function show_login(){
+    $(".signup").hide();
+    $(".btn2r").show();
+    $(".btn2l").hide();
+    $(".login").show();
+}
+
